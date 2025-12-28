c5e7f701ca5924b830d8ff9f83df8ba7e1c7bba2
Andrew Gleason <angleaso@cisco.com>
2025-12-28 16:58:04 -0500

    add page trackign hook

---
+++ src/hooks/usePageTracking.ts
@@ -0,0 +1,30 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
+
+declare global {
+  interface Window {
+    gtag: (command: string, ...args: any[]) => void
+  }
+}
+
+const pageTitles: Record<string, string> = {
+  '/': 'apg.',
+  '/about': 'apg. | about.',
+  '/contact': 'apg. | contact.',
+}
+
+export function usePageTracking() {
+  const location = useLocation()
+
+  useEffect(() => {
+    const title = pageTitles[location.pathname] || 'apg.'
+    document.title = title
+
+    if (typeof window.gtag === 'function') {
+      window.gtag('event', 'page_view', {
+        page_path: location.pathname + location.hash,
+        page_title: title,
+      })
+    }
+  }, [location])
+}
